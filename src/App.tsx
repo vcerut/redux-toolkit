4c7c50ca87ed9bc0e5f2028ad12bc3c7efdf7df4
vcerut <97165687+vcerut@users.noreply.github.com>
2023-11-07 13:20:21 +0100

    added features

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,22 @@ function App() {
   // const [count, setCount] = useState(0);
   const count = useAppSelector((state) => state.counter.counter);
   const dispatch = useAppDispatch();
+  const doubleButton = useAppSelector((state) => state.counter.double);
 
   return (
     <>
       <div className="card">
-        <button onClick={() => dispatch(counterActions.increment())}>
-          count is {count}
+        <h1>Count is: {count}</h1>
+        <button onClick={() => dispatch(counterActions.increment())}>+1</button>
+        <button onClick={() => dispatch(counterActions.decrement())}>-1</button>
+        <button
+          onClick={() => dispatch(counterActions.doubleToggle())}
+          className={doubleButton ? "active" : "inactive"}
+          disabled={count == 0}
+        >
+          DOUBLE
         </button>
+        <button onClick={() => dispatch(counterActions.reset())}>RESET</button>
       </div>
     </>
   );
--- src/redux/slices/counter.slice.ts
+++ src/redux/slices/counter.slice.ts
@@ -2,10 +2,12 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export type CounterState = {
   counter: number;
+  double: boolean;
 };
 
 const initialState: CounterState = {
   counter: 0,
+  double: false,
 };
 
 //definizione della slice di Redux
@@ -14,8 +16,31 @@ export const counterSlice = createSlice({
   initialState: initialState,
   reducers: {
     increment: (state) => {
-      //incremento di valore
-      state.counter += 1; //state.counter = state.counter + 1
+      if (state.double == false) {
+        //incremento di valore
+        state.counter += 1; //state.counter = state.counter + 1
+      } else {
+        //raddoppia
+        state.counter = state.counter * 2;
+      }
+    },
+    decrement: (state) => {
+      if (state.double == false) {
+        //decremento di valore
+        state.counter -= 1; //state.counter = state.counter - 1
+      } else {
+        //dimezza
+        state.counter = state.counter / 2;
+      }
+    },
+    reset: (state) => {
+      //reset del valore
+      state.counter = 0;
+    },
+    doubleToggle: (state) => {
+      if (state.counter != 0) {
+        state.double = !state.double;
+      }
     },
   },
 });
